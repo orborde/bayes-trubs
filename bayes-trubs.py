c1e1978f2b23af19d99d70a04f94a029a302221e
William Ehlhardt <williamehlhardt@gmail.com>
2019-12-22 17:13:23 -0800

    plot f(x)

---
+++ bayes-trubs.py
@@ -0,0 +1,29 @@
+#! /usr/bin/env python3
+
+import math
+import matplotlib.pyplot as plt
+
+def f(x):
+    assert x >= 0 and x <= 1
+
+    if x < 1/2:
+        return math.log2(math.log2(1/abs(x - 1/4)))
+    else:
+        return 0.25 / abs(x - 3/4)
+
+
+points = []
+STEPS=1000
+for x in range(0, STEPS+1):
+    x = x/STEPS
+    try:
+        y = f(x)
+        points.append( (x,y) )
+    except (ValueError, ZeroDivisionError) as e:
+        print(x, e)
+
+print(points)
+xs,ys = zip(*points)
+plt.plot(xs, ys)
+plt.axis([0,1, 0,30])
+plt.show()
