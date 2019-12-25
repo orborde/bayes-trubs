a46f803a0bf5949c4870e80042df81ffe0b45f79
William Ehlhardt <williamehlhardt@gmail.com>
2019-12-24 16:35:04 -0800

    represent infinity explicitly

--- bayes-trubs.py
+++ bayes-trubs.py
@@ -6,7 +6,9 @@ import matplotlib.pyplot as plt
 def f(x):
     assert x >= 0 and x <= 1
 
-    if x < 1/2:
+    if x == 1/4 or x == 3/4:
+        return math.inf
+    elif x < 1/2:
         return math.log2(math.log2(1/abs(x - 1/4)))
     else:
         return 0.25 / abs(x - 3/4)
@@ -16,13 +18,9 @@ points = []
 STEPS=1000
 for x in range(0, STEPS+1):
     x = x/STEPS
-    try:
-        y = f(x)
-        points.append( (x,y) )
-    except (ValueError, ZeroDivisionError) as e:
-        print(x, e)
+    y = f(x)
+    points.append( (x,y) )
 
-print(points)
 xs,ys = zip(*points)
 plt.plot(xs, ys)
 plt.axis([0,1, 0,30])
