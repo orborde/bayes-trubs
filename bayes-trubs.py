ceb21a3c068cf0df2a36ab6e7e0f97c37e34b208
William Ehlhardt <williamehlhardt@gmail.com>
2019-12-24 17:23:09 -0800

    implement posterior

--- bayes-trubs.py
+++ bayes-trubs.py
@@ -3,6 +3,8 @@
 import math
 import matplotlib.pyplot as plt
 
+import geom
+
 def f(x):
     assert x >= 0 and x <= 1
 
@@ -13,15 +15,32 @@ def f(x):
     else:
         return 0.25 / abs(x - 3/4)
 
+def trunc_geom_pmf(k, p):
+    if k > f(p):
+        return 0
 
-points = []
-STEPS=1000
-for x in range(0, STEPS+1):
-    x = x/STEPS
-    y = f(x)
-    points.append( (x,y) )
+    return geom.pmf(k, p)
 
-xs,ys = zip(*points)
-plt.plot(xs, ys)
-plt.axis([0,1, 0,30])
-plt.show()
+def posterior(p, samples):
+    pdf = 1
+    for k in samples:
+        pdf *= trunc_geom_pmf(k, p)
+    return pdf
+
+samples = []
+while True:
+    sample = geom.sample(1/4)
+    print('sampled', sample)
+    samples.append(sample)
+
+    points = []
+    STEPS=1000
+    for x in range(0, STEPS+1):
+        p = x/STEPS
+        y = posterior(p, samples)
+        points.append( (p,y) )
+
+    xs,ys = zip(*points)
+    plt.plot(xs, ys)
+    plt.waitforbuttonpress()
+    plt.close()
